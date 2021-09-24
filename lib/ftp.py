81b029d5457cfb72b5b9e9c788b726ee95bacad6
Aditya R Pai <41117722+adityarpai843@users.noreply.github.com>
2021-09-24 16:18:46 +0530

    Added a feature

--- lib/ftp.py
+++ lib/ftp.py
@@ -1,14 +1,14 @@
 from ftplib import FTP
 class FTPScan:
 
-    def __init__(self,ip,verbosity):
+    def __init__(self,ip,verbosity=False):
         self.verbosity = verbosity
         self.ip = ip
         self.ftp = FTP(self.ip)
     
     def anonymous_login(self):
         resp = self.ftp.login()
-        if resp =='230 Login successful':
+        if resp =='230 Login successful.':
             print("FTP Anonymous Login Allowed !!")
         else:
             print("Failed")     

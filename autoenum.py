b8304215313647a5b6d338c05ac075e0d50079ba
Aditya R Pai <41117722+adityarpai843@users.noreply.github.com>
2021-09-24 16:37:21 +0530

    minor changes

--- autoenum.py
+++ autoenum.py
@@ -1,6 +1,6 @@
 import argparse
 from lib.ftp import FTPScan
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description="AutoEnum\n\nEnumerating well known ports to find vulnerabilities automatically",epilog="Ex: python3 autoenum.py 9.0.9.0 22,21\n")
+parser = argparse.ArgumentParser(prog="autoenum",formatter_class=argparse.RawDescriptionHelpFormatter,description="AutoEnum\n\nEnumerating well known ports to find vulnerabilities automatically",epilog="Ex: python3 autoenum.py 9.0.9.0 22,21\n")
 parser.add_argument("ip",help="Enter host IP address")
 parser.add_argument("ports", help="Enter the ports separated by comma")
 parser.add_argument("-v","--verbose",help="make output verbose",action="store_true")
